6bbd5c5bf850371e9d8e525692d39db895e6967d
ruchin2000 <159120165+ruchin2000@users.noreply.github.com>
2024-08-11 16:55:19 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import { View, Text } from 'react-native';
+import AppNavigator from './ios/Src/Navigation/AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+
+const App = () => {
+  return (
+    
+   <AppNavigator/>
+  )
+}
+
+
+
+export default App;
